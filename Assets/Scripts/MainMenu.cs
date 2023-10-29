9e3ddfebb128a7da9b3d9eadd33b1c33d79070e2
ogike <tasi.zoltan07@gmail.com>
2023-10-29 18:26:17 +0100

    Main menu new picture

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,38 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject creditsPanel;
+    public GameObject controlsPanel;
+
+    public void Start()
+    {
+        creditsPanel.SetActive(false);
+        controlsPanel.SetActive(false);
+    }
+
+    public void ToggleCredits()
+    {
+        bool newValue = !creditsPanel.activeInHierarchy;
+        creditsPanel.SetActive(newValue);
+
+        if (newValue == true)
+            controlsPanel.SetActive(false);
+    }
+    
+    public void ToggleControls()
+    {
+        bool newValue = !controlsPanel.activeInHierarchy;
+        controlsPanel.SetActive(newValue);
+        
+        if (newValue == true)
+            creditsPanel.SetActive(false);
+    }
+    
     public void StartGame()
     {
         //1 has to be the main gameplay loop
         SceneManager.LoadScene(1);
     }
+    
+    
 }
